473cb4a451d306970e903f880a795ae0a81b01f9
Asa Toburen <atopettit@gmail.com>
2015-03-10 19:12:14 -0700

    changes

--- script.js
+++ script.js
@@ -7,15 +7,8 @@ app.directive('optIn', function() {
   return {
     templateUrl: 'opt-in.html',
     restrict: 'E',
-    replace: false,
+    replace: true,
     transclude: true
   };
 });
 
-
-app.directive('loginForm', function() {
-  return {
-    restrict: 'E',
-    template: '<opt-in><form method="POST" accept-charset="utf-8"><input type="text" name="firstName" placeholder="first name" id="" value="" /><input type="text" name="lastName" placeholder="last name" id="" value="" /><input type="text" name="email" placeholder="email"  id="email" value="" /><input type="submit" name="submit" value="Submit Your Info" /></form></opt-in>',
-  };
-});
